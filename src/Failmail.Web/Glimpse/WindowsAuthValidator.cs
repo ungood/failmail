9512be6759dbf749be8de0c0554017b56873e480
Jason Walker <ungood@onetrue.name>
2012-08-22 15:51:03 -0600

    installed glimpse, removed packages

---
+++ src/Failmail.Web/Glimpse/WindowsAuthValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Validator;
+
+namespace Failmail.Web.Glimpse
+{
+    [GlimpseValidator]
+    public class WindowsAuthValidator : IGlimpseValidator
+    {
+        public bool IsValid(HttpContextBase context, LifecycleEvent lifecycleEvent)
+        {
+            return true;
+        }
+    }
+}
